596e2db0120ed7474d4fab8089cc7d703f2e5336
Andrew Gulik <andrewgulik@gmail.com>
2017-05-08 18:34:39 -0400

    updated the bookmarks controller and completed the methods on the dashboardlyDataLoader module.

--- controllers/bookmarks.js
+++ controllers/bookmarks.js
@@ -7,14 +7,27 @@ module.exports = (dataLoader) => {
 
   // Modify a bookmark
   bookmarksController.patch('/:id', onlyLoggedIn, (req, res) => {
-    // TODO: this is up to you to implement :)
+    dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id)
+    .then(() => {
+      return dataLoader.updateBookmark(req.params.id, {
+        title: req.body.title,
+        url: req.body.url
+      });
+    })
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json(err));
     res.status(500).json({ error: 'not implemented' });
   });
 
 
   // Delete a bookmark
   bookmarksController.delete('/:id', onlyLoggedIn, (req, res) => {
-    // TODO: this is up to you to implement :)
+    dataLoader.bookmarkBelongsToUser(req.params.id, req.user.id)
+    .then(() => {
+      return dataLoader.deleteBookmark(req.params.id);
+    })
+    .then(() => res.status(204).end())
+    .catch(err => res.status(400).json(err));
     res.status(500).json({ error: 'not implemented' });
   });
 
